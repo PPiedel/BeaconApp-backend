01b1313dab3559e2e9694a6874a97b65b19d644a
ppiedel <pawelpiedel@yahoo.pl>
2018-09-20 22:24:26 +0200

    Init application with h2 in memory db

---
+++ src/main/java/pl/yahoo/pawelpiedel/beaconbackend/BeaconBackendApplication.java
@@ -0,0 +1,12 @@
+package pl.yahoo.pawelpiedel.beaconbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeaconBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeaconBackendApplication.class, args);
+	}
+}
